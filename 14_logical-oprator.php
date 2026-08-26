378e0672035504221a25653e37ed30ab149cd614
siddheshwarmunde <siddheshwarmunde01@gmai.com>
2026-08-26 12:38:22 +0530

    logical operator and for loop

---
+++ 14_logical-oprator.php
@@ -0,0 +1,25 @@
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+
+if($a == 10 && $b == 20){
+  echo "true condition";
+}
+
+if($a == 10 and $b == 20){
+  echo "true condition";
+}
+
+if($a == 10 or $b ==30){
+  echo "true condition";
+}
+
+if($a == 10 xor $b==100){    // one true then execute, not when both false or both true
+  echo "true condition";
+}
+
+if($a != 20){
+echo "true condition";
+}
+?>
---
+++ 15_for-loop.php
@@ -0,0 +1,14 @@
+<?php
+$table = 19;
+
+for($i=1; $i <=10; $i++){
+  echo "siddheshwar $i"; 
+  echo "<br>";
+}
+
+for($i =1; $i <=10; $i++){
+  echo $table * $i;
+  echo "<br>";
+}
+?>
+
