e43b7c3b285229389ddddd995957e8a1d66ed287
siddheshwarmunde <siddheshwarmunde01@gmai.com>
2026-08-26 11:18:22 +0530

    if else condition

---
+++ 12_if-else-condition.php
@@ -0,0 +1,35 @@
+<?php
+$age = 25;
+$name = "siddheshwar munde";
+
+if($age > 18){
+  echo "he / she can vote in indian election";
+}
+else{
+  echo "minor, can not vote";
+}
+?>
+
+"<br/>";
+"<br/>";
+"<br/>";
+
+<?php
+ $country = "USA";
+
+ if($country == "india"){
+  echo "its an asian country";
+ }
+
+ else if($country == "USA"){
+  echo "its an ameriacan country";
+ }
+
+ else if ($country == "japan"){
+  echo "its a asian pacific country";
+ }
+ else {
+  echo "unknown country";
+ }
+
+?>
