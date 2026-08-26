0c5e43b3704e70a6cbee7484f0b627ba63d20e4c
siddheshwarmunde <siddheshwarmunde01@gmai.com>
2026-08-26 11:59:54 +0530

    switch statement

---
+++ 13_switch.php
@@ -0,0 +1,27 @@
+<?php
+
+$day = "wensday";
+
+switch($day){
+  case "monday":
+    echo "white shirt and grey pants";
+    break;
+
+  case "tuesday":
+    echo "sky blue shirt and beige pant";
+    break;
+
+  case "wensday":
+      echo "black shirt and white pant";
+      break;
+
+  case "thirsday":
+        echo "maroon shirt and black pant";
+        break;
+
+  default :
+  echo "holiday";
+  break;
+
+  
+}
