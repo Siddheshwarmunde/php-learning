042356441f164a143dcb7d6a9e1eac86b6bbcfd3
siddheshwarmunde <siddheshwarmunde01@gmai.com>
2026-08-26 15:35:42 +0530

    while loop and do while loop

---
+++ 18_while-loop.php
@@ -0,0 +1,23 @@
+<?php
+
+$num = 0;
+
+while($num < 10){
+  echo $num;
+  echo "<br>";
+  $num++;
+}
+?>
+
+<?php
+echo "<hr>";
+
+$num1 = 10;
+
+while($num1 > 0){
+  echo $num1;
+  echo "<br>";
+  $num1--;
+}
+
+?>
---
+++ 19_do-while-loop.php
@@ -0,0 +1,27 @@
+<?php
+
+$num=0;
+
+do{
+ 
+  echo "$num";
+  echo "<br>";
+   $num++;
+
+}while( $num <= 10)
+
+
+?>
+
+<?php
+echo "<hr>";
+
+$num = 10;
+do{
+  echo $num;
+  echo "<br>";
+
+  $num--;
+}while($num > 0);
+
+?>
