069390ec854cc13f239e5ed2d0647cfec609d39d
siddheshwarmunde <siddheshwarmunde01@gmai.com>
2026-08-26 07:53:41 +0530

    update files

--- 00_index.php
+++ 00_index.php
@@ -1,4 +1,8 @@
 <?php
 echo 30/10;
-
+/*
+This is a
+multi-line comment
+*/
 ?>
+
--- 02_variables.php
+++ 02_variables.php
@@ -3,10 +3,17 @@ $name = "siddheshwar";
 $NAME = "munde";
 $Name = "sam";
 $namE = "siddheshwar_munde";
+$age = 26;
+$salary = 40000;
+$price = 99.80;
+
 
 
 echo $namE;
 echo $name;
 echo $NAME;
 echo $Name;
+echo $age;
+echo $salary;
+echo $price;
 ?>
