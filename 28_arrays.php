018ba1a4147f2ce2fe0a78f181069c2ca8f49c51
siddheshwarmunde <siddheshwarmunde01@gmai.com>
2026-08-27 07:30:24 +0530

    array and for each loop

---
+++ 28_arrays.php
@@ -0,0 +1,16 @@
+<?php
+
+$users = ["sam","bruce", "john", "anthany", "peter", "soidrman"];
+
+// echo $users[1];
+// echo $users[0];
+// echo $users[2];
+
+// echo count($users);
+
+for($user=0; $user < count($users); $user++){
+ echo $users[$user];
+ echo "<br>";
+}
+
+?>
---
+++ 29_for-each-loop.php
@@ -0,0 +1,26 @@
+<?php
+$users = ["japan", "australia", "china", "india", "pak", "usa", "russia"];
+
+foreach($users as $x){
+  
+
+  if($x == "pak"){
+       continue;
+  }
+
+  echo $x;
+  echo "<br>";
+
+
+//  if($x == "india"){
+// break;
+//}
+
+}
+
+foreach($users as $x ):
+echo $x;
+echo "<br>";
+endforeach;
+
+?>
